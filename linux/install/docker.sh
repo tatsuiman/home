964ead7cbad65de053ade816e47ce32d0baf2452
tatsui <tatsuya.ito.at@gmail.com>
2019-05-03 20:10:18 +0900

    update script

--- linux/install/docker.sh
+++ linux/install/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install docker
+sudo apt install docker.io
 sudo usermod -aG docker ${USER}
-sudo curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod 755 /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
