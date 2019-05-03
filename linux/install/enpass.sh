f18fcacb8b9930b9e966e08b56b569a34230370f
tatsui <tatsuya.ito.at@gmail.com>
2019-05-04 02:22:11 +0900

    update script

--- linux/install/enpass.sh
+++ linux/install/enpass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "deb https://apt.enpass.io/ stable main" | sudo tee -a /etc/apt/sources.list.d/enpass.list
 wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo apt-key add -
-apt-get update
-apt-get install enpass
+sudo apt-get update
+sudo apt-get install enpass
