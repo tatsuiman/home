94fe81f46d46ddb21a9175a102b3f6b762855676
tatsui <2360691+tatsuiman@users.noreply.github.com>
2025-02-08 00:12:38 +0900

    Update mac setup script with new brew packages

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,4 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install cask
 brew install --cask slack notion blender bambu-studio zoom enpass microsoft-edge firefox
-brew install jq tree fzf direnv z pyenv starship awscli nmap ollama ffmpeg
+brew install jq tree fzf direnv z pyenv starship awscli nmap ollama ffmpeg terrafrom
