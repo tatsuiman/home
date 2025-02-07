0df89d34519741fa247a2643f69773710a8083d9
tatsui <2360691+tatsuiman@users.noreply.github.com>
2025-02-08 00:12:14 +0900

    Update setup.sh with new brew installations for apps

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,11 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install starship
-brew install direnv
-brew install neovim
 brew install cask
-brew install yubico-piv-tool
-brew install opensc
-brew install virtualenvwrapper
-brew install ffmpeg
-brew install --cask slack
-brew install tree fzf
+brew install --cask slack notion blender bambu-studio zoom enpass microsoft-edge firefox
+brew install jq tree fzf direnv z pyenv starship awscli nmap ollama ffmpeg
